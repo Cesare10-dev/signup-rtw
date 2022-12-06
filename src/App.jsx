0c7171557f9a1975b6cd8661f4353d62b2ce041c
Cesare10-dev <cesare1087@gmail.com>
2022-12-06 19:22:49 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Signup from "./Signup";
 
 function App() {
   return (
-    <div className="flex flex-col justify-center h-screen items-center bg-green-300">
+    <div className="flex flex-col justify-center h-screen items-center bg-gradient-to-b from-lime-200 to-lime-600">
       <Signup />
     </div>
   );
